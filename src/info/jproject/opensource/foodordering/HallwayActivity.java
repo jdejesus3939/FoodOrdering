6ba31bfbc656b5382ff7c5b0e1ef46c3b3e69afc
jemerensiana <koko_ang@yahoo.com>
2014-02-05 23:19:29 -0500

    Updated Hallway Activity with custom adapter LV

--- src/info/jproject/opensource/foodordering/HallwayActivity.java
+++ src/info/jproject/opensource/foodordering/HallwayActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HallwayActivity extends Activity {
 
@@ -36,11 +38,11 @@ public class HallwayActivity extends Activity {
 				getApplicationContext(), food_products, icon_food_products);
 
 		// must call recycle() after done using obtainTypedArray
-		icon_food_products.recycle();
+		//icon_food_products.recycle();
 
 		// Get the login session id
-		Intent i = getIntent();
-		PHPSESSID = i.getStringExtra("Set-Cookie");
+		//Intent i = getIntent();
+		//PHPSESSID = i.getStringExtra("Set-Cookie");
 
 		// Binding Array to ListAdapter
 		final ListView list = (ListView) findViewById(android.R.id.list);
@@ -50,95 +52,90 @@ public class HallwayActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 
-				// selected item
-				String product = ((TextView) view).getText().toString();
+				//Toast.makeText(getApplicationContext(), Integer.toString(position), Toast.LENGTH_SHORT).show();
 
-				char num = product.charAt(0);
+				switch (position) {
+				case 0:
+					// Launching new Activity on selecting single List Item
+					Intent univcenterInfo = new Intent(getApplicationContext(),
+							UnivCenterInfoActivity.class);
+					// univcenterInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					univcenterInfo.putExtra("Set-Cookie", PHPSESSID);
+					univcenterInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(univcenterInfo);
+					break;
 
-				switch (num) {
-				case '1':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent univcenterInfo = new
-					 * Intent(getApplicationContext(),
-					 * UnivCenterInfoActivity.class);
-					 * //univcenterInfo.addFlags(Intent
-					 * .FLAG_ACTIVITY_CLEAR_TOP); // sending data to new
-					 * activity univcenterInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * univcenterInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID);
-					 * startActivity(univcenterInfo); break;
-					 */
-				case '2':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent subwayInfo = new Intent(getApplicationContext(),
-					 * SubwayInfoActivity.class);
-					 * //subwayInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //
-					 * sending data to new activity
-					 * subwayInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * subwayInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID); startActivity(subwayInfo);
-					 * break;
-					 */
-				case '3':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent einsteinbrosInfo = new
-					 * Intent(getApplicationContext(),
-					 * EinsteinBrosInfoActivity.class);
-					 * //einsteinbrosInfo.addFlags
-					 * (Intent.FLAG_ACTIVITY_CLEAR_TOP); // sending data to new
-					 * activity
-					 * einsteinbrosInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * einsteinbrosInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID);
-					 * startActivity(einsteinbrosInfo); break;
-					 */
-				case '4':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent ultimatebajaInfo = new
-					 * Intent(getApplicationContext(),
-					 * UltimateBajaInfoActivity.class);
-					 * //ultimatebajaInfo.addFlags
-					 * (Intent.FLAG_ACTIVITY_CLEAR_TOP); // sending data to new
-					 * activity
-					 * ultimatebajaInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * ultimatebajaInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID);
-					 * startActivity(ultimatebajaInfo); break;
-					 */
-				case '5':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent simplytogoInfo = new
-					 * Intent(getApplicationContext(),
-					 * SimplyToGoInfoActivity.class);
-					 * //simplytogoInfo.addFlags(Intent
-					 * .FLAG_ACTIVITY_CLEAR_TOP); // sending data to new
-					 * activity simplytogoInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * simplytogoInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID);
-					 * startActivity(simplytogoInfo); break;
-					 */
-				case '6':
-					/*
-					 * // Launching new Activity on selecting single List Item
-					 * Intent starbucksInfo = new
-					 * Intent(getApplicationContext(),
-					 * StarbucksInfoActivity.class);
-					 * //starbucksInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-					 * // sending data to new activity
-					 * starbucksInfo.putExtra("Set-Cookie",PHPSESSID);
-					 * starbucksInfo.putExtra("prod", product);
-					 * Log.e("Set-Cookie",PHPSESSID);
-					 * startActivity(starbucksInfo); break;
-					 */
+				case 1:
+					// Launching new Activity on selecting single List Item
+					Intent subwayInfo = new Intent(getApplicationContext(),
+							SubwayInfoActivity.class);
+					// subwayInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					subwayInfo.putExtra("Set-Cookie", PHPSESSID);
+					subwayInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(subwayInfo);
+					break;
+
+				case 2:
+					// Launching new Activity on selecting single List Item
+					Intent einsteinbrosInfo = new Intent(
+							getApplicationContext(),
+							EinsteinBrosInfoActivity.class);
+					// einsteinbrosInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					einsteinbrosInfo.putExtra("Set-Cookie", PHPSESSID);
+					einsteinbrosInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(einsteinbrosInfo);
+					break;
+
+				case 3:
+					// Launching new Activity on selecting single List Item
+					Intent ultimatebajaInfo = new Intent(
+							getApplicationContext(),
+							UltimateBajaInfoActivity.class);
+					// ultimatebajaInfo.addFlags
+					// (Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					ultimatebajaInfo.putExtra("Set-Cookie", PHPSESSID);
+					ultimatebajaInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(ultimatebajaInfo);
+					break;
+
+				case 4:
+					// Launching new Activity on selecting single List Item
+					Intent simplytogoInfo = new Intent(getApplicationContext(),
+							SimplyToGoInfoActivity.class);
+					// simplytogoInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					simplytogoInfo.putExtra("Set-Cookie", PHPSESSID);
+					simplytogoInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(simplytogoInfo);
+					break;
+
+				case 5:
+					// Launching new Activity on selecting single List Item
+					Intent starbucksInfo = new Intent(getApplicationContext(),
+							StarbucksInfoActivity.class);
+					// starbucksInfo.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					// sending data to new activity
+					starbucksInfo.putExtra("Set-Cookie", PHPSESSID);
+					starbucksInfo.putExtra("prod", Integer.toString(position));
+					//Log.e("Set-Cookie", PHPSESSID);
+					startActivity(starbucksInfo);
+					break;
+
 				default:
 					break;
 				}
+
 			}
+			
 		});
 
 	}
